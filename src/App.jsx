fc1fd49db088487fa4ecf32a875c96f463e77b44
VikrantPrajapati <prajapati865039@gmail.comA>
2024-02-11 11:44:12 +0530

    Order todo list

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react'
+import React from 'react';
+import reactLogo from './assets/react.svg'
+import './App.css'
+import axios from 'axios';
+import CheckResponse from './CheckResponse';
+
+function App() {
+  const [orderData,setorderData] = useState([]);
+  useEffect(()=>{
+    getdata();
+   
+  },[])
+  async function getdata(){
+    const data = await axios.get('https://purple-journalist-dmoxo.pwskills.app:5000/Meesho/ShowOrder');
+    setorderData(data.data);
+    console.log(data.data)
+  }
+
+  return (
+   <>
+    {orderData.map((elem,ind)=>{
+      return (
+        <>
+          <div className="Order">
+            <div className="orderItem">Order-{ind+1}</div>
+             <CheckResponse MobileNO = {elem.MobileNO} Responses = {elem.Responses} index = {ind}/>
+            
+           <div className="MobileNo">MobileNo:{elem.MobileNO}</div>
+           <hr />
+           
+           <div className="products">
+            <div className="productdata">
+              <div className="productitem"></div>
+              <div className="productitem">ProductImage</div>
+              <div className="productitem">ProductName</div>
+              <div className="productitem">Price</div>
+              <div className="productitem">Qty</div>
+            </div>
+            {elem.Products.map((elem)=>{
+              return (
+                <div className="productdata">
+                  <div className="productitem"><input className='checkbox' type="checkbox"/></div>
+                  <div className="productitem"><img className='ProductImage' src={elem.Url} alt="ProductImage" /></div>
+                  <div className="productitem">{elem.ProductName}</div>
+                  <div className="productitem">{elem.Price}</div>
+                  <div className="productitem">{elem.Qty}</div>
+                </div>
+              )
+            })}
+           </div>
+          </div>
+          </>
+      )
+    })}
+   </>
+  )
+}
+
+export default App
---
+++ src/CheckResponse.jsx
@@ -0,0 +1,55 @@
+import axios from "axios";
+import React from "react";
+import { useEffect } from "react";
+function CheckResponse(props){
+    useEffect(()=>{
+        setTimeout(() => {
+            checkres1();
+            checkres2();
+            checkres3();
+            checkres4();
+        }, 2000);
+      
+       
+            
+    })
+    function checkres1(){
+        
+            if(props.Responses.Seen === true){
+                document.getElementById(`response_seen${props.index}`).click()
+               }
+           
+    }
+    function checkres2(){
+         if(props.Responses.Confirmation === true){
+            document.getElementById(`response_confirm${props.index}`).click()
+           }
+    }
+    function checkres3(){
+         if(props.Responses.Ready === true){
+            document.getElementById(`response_ready${props.index}`).click();
+           }
+    }
+    function checkres4(){
+         if(props.Responses.OutForDelivery === true){
+            document.getElementById(`response_outFordelivery${props.index}`).click()
+           }
+    }
+   async function Sendresponse(type){
+    console.log(type)
+        await axios.put(`https://purple-journalist-dmoxo.pwskills.app:5000/Meesho/updateOrderResponse/${props.MobileNO}/${type}`);
+
+    }
+
+       return (
+        <div className="Responses">
+        <div className="responsitem"><div>Seen</div><input className='checkbox' type="checkbox" name="" id={`response_seen${props.index}`} onClick = {()=>Sendresponse('Seen')}/></div>
+        <div className="responsitem"><div>Confirmation</div><input className='checkbox' type="checkbox" name="" id={`response_confirm${props.index}`} onClick = {()=>Sendresponse('Confirmation')} /></div>
+        <div className="responsitem"><div>Ready</div><input className='checkbox' type="checkbox" name="" id={`response_ready${props.index}`} onClick = {()=>Sendresponse('Ready')} /></div>
+        <div className="responsitem"><div>OutForDelivery</div><input className='checkbox' type="checkbox" name="" id={`response_outFordelivery${props.index}`} onClick = {()=>Sendresponse('OutForDelivery')} /></div>
+        <div className="responsitem"><div>Complete</div><input className='checkbox' type="checkbox" name="" id={`response_complete${props.index}`} onClick = {()=>Sendresponse('Complete')} /></div>
+       
+      </div>
+       )
+}
+export default CheckResponse
